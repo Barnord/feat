96c6b7f2b14f2e8fbc9a94bda3fa416e9178344e
Ben Arno <bdarno92@gmail.com>
2021-04-21 16:14:53 -0700

    fixed it

--- app1.js
+++ app1.js
@@ -2,7 +2,7 @@ let userRace = prompt('What\'s your favorite 5e race?');
 let greeting;
 
 if (userRace.toLowerCase() == 'variant human') {
-    greeting = 'eat shit weenie';
+    greeting = 'bad choice';
 }   else {greeting = 'I too enjoy ' + userRace + '.'; }
 
 document.write('<h1>' + greeting + '</h1>');
