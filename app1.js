2b85365625bcc2d8009832b35a8a8658ef161cda
Ben Arno <bdarno92@gmail.com>
2021-04-22 13:06:35 -0700

    updated for lab 7

--- app1.js
+++ app1.js
@@ -1,12 +1,34 @@
 'use strict'
 
-let userRace = prompt('What\'s your favorite 5e race?');
 
+function getRace() {
+    let userRace = prompt('What\'s your favorite 5e race?');
+    let greet;
+    if (userRace.toLowerCase() == 'variant human') {
+        greet = 'How original.';
+    }   else greet = ('I too enjoy ' + userRace + '.'); 
+    document.write('<h3>' + greet + '</h3>')}
 
-if (userRace.toLowerCase() == 'variant human') {
-    greeting = 'How original.';
-}   else {greeting = ('I too enjoy ' + userRace + '.'); }
 
 // Asks the user what race they enjoy in 5e, changes greeting accordingly.
 
-document.write('<h3>' + greeting + '</h3>')
+
+function dynamicGreet() {
+    let today = new Date();
+let hourNow = today.getHours();
+let greeting;
+if (hourNow > 18) {
+    greeting = 'Good Evening';
+} else if (hourNow > 12) {
+    greeting = 'Good Afternoon';
+} else if (hourNow > 0) {
+    greeting = 'Good Morning';
+} else {
+    greeting = 'Welcome';
+}
+document.write('<h3>' + greeting + '</h3>');
+}
+
+
+function askCookies(){
+    cookies = confirm('Would you be willing to sign a petition to ban the race Variant Human from Fifth Edition Dungeons and Dragons?')}
