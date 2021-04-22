e9055c055660c8238d0de83c3ba055f1e487f89a
Ben Arno <bdarno92@gmail.com>
2021-04-21 22:41:32 -0700

    added app2.js app3.js, made app1 print properly

--- app1.js
+++ app1.js
@@ -1,8 +1,12 @@
+'use strict'
+
 let userRace = prompt('What\'s your favorite 5e race?');
-let greeting;
+
 
 if (userRace.toLowerCase() == 'variant human') {
-    greeting = 'bad choice';
-}   else {greeting = 'I too enjoy ' + userRace + '.'; }
+    greeting = 'How original.';
+}   else {greeting = ('I too enjoy ' + userRace + '.'); }
 
-document.write('<h1>' + greeting + '</h1>');
+// Asks the user what race they enjoy in 5e, changes greeting accordingly.
+
+document.write('<h3>' + greeting + '</h3>')
---
+++ app3.js
@@ -0,0 +1,3 @@
+'use strict'
+
+cookies = confirm('Would you be willing to sign a petition to ban the race Variant Human from Fifth Edition Dungeons and Dragons?')
